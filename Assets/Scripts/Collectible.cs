aa4b57513c7f75e311fd817066dfbd26a8d78d27
oscarBustos <oscaribh2011@gmail.com>
2023-12-25 20:35:24 +0100

    A prototyping scene was created, additionally, a gem sprite was imported and the onTriggerEnter2D was written to destroy the object once it is collected, missing - integration with player Script and play sound when collected

---
+++ Assets/Scripts/Collectible.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectible : MonoBehaviour
+{
+
+    [SerializeField] private CollectibleType collectibleType;
+
+    #region Collisions
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        // Todo: When collision with Player, execute player's collect method.
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            //collision.gameObject.GetComponent<Player>().CollectObject(collectableType, );
+            // Todo: play sound 
+            gameObject.SetActive(false);
+            Destroy(gameObject, 0.5f);
+        }
+    }
+    #endregion
+}
--- Assets/Scripts/CollectibleType.cs
+++ Assets/Scripts/CollectibleType.cs
@@ -2,8 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum CollectableType
+public enum CollectibleType
 {
 
-    Health,
+    Health, 
+    Point
 }
