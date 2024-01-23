16839d114f0198b559c9ba023a251b8d7305e8a0
oscarBustos <oscaribh2011@gmail.com>
2024-01-23 12:13:12 +0100

    Level02 initialization, and some moving platforms are being created

---
+++ Assets/Scripts/PlatformMovement.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+public class PlatformMovement : MonoBehaviour
+{
+    [SerializeField] int horizontalDirection;
+    [SerializeField] float maxHorizontalDistance;
+    [SerializeField] int verticalDirection;
+    [SerializeField] float maxVerticalDistance;
+    [SerializeField] float speed;
+    
+    
+
+    private Vector3 originalPosition;
+    private Vector3 targetPosition;
+    private bool moving;
+    
+    private void Awake()
+    {
+        originalPosition = transform.position;
+    }
+    
+    void Update()
+    {
+        if (!moving)
+        {
+            float x = originalPosition.x + maxHorizontalDistance * horizontalDirection;
+            float y = originalPosition.y + maxVerticalDistance * verticalDirection;
+            targetPosition = new Vector3(x, y, 0);
+            moving = true;
+            Debug.Log("Current" + originalPosition);
+            Debug.Log("Target" + targetPosition);
+        } 
+        else if (transform.position == targetPosition) {
+            horizontalDirection *= -1;
+            verticalDirection *= -1;
+            moving = false;
+            originalPosition = transform.position;
+            Debug.Log("Current" + originalPosition);
+            Debug.Log("Target" + targetPosition);
+        } else if(moving)
+        {
+            Debug.Log("Moving" + targetPosition);
+            transform.position = Vector2.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
+        }
+    }
+
+    #region Collisions
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            collision.gameObject.transform.parent = transform;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            collision.gameObject.transform.parent = null;
+        }
+    }
+    #endregion
+}
