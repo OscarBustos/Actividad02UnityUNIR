1d40a02acae11110f41b714b37f2a9bbfc3c116b
Laura Mambrilla Moreno <lauramambrilla@gmail.com>
2024-01-24 22:47:22 +0100

    Player's jump sound

--- Assets/Scripts/CharaterController.cs
+++ Assets/Scripts/CharaterController.cs
@@ -32,7 +32,7 @@ public class CharaterController : MonoBehaviour
     /// Methods
     /// ------------------------------------------------------------------------------------------------------------------------
 
-    private void PlaySound()
+    protected void PlaySound()
     {
         sound.Play();
     }
@@ -41,19 +41,11 @@ public class CharaterController : MonoBehaviour
     {
         if (direction < 0)
         {
-            if (!spriteRenderer.flipX)
-            {
-                PlaySound();
-            }
             spriteRenderer.flipX = true;
             isRight = true;
         }
         else if (direction > 0)
         {
-            if (spriteRenderer.flipX)
-            {
-                PlaySound();
-            }
             spriteRenderer.flipX = false;
             isRight = false;
         }
